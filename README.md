614612fc5b473348500f049bdcdc1972b49d72ee
Russell Pacheco <russ1487@gmail.com>
2021-05-31 11:03:01 +0900

    pushing latest version. Currently implementing api endpoints

--- README.md
+++ README.md
@@ -7,3 +7,28 @@
 - David
 - Russell
 - Julie
+
+##Getting Up and Running
+
+1. Clone Repo
+
+1. Create a virtual environment
+    `
+    python3 -m venv <FOLDERNAME>
+    `
+1. Start the virtual environment
+
+    For Mac Users
+    `
+    source /<VENV FOLDER>/bin/activate
+    `
+
+    For Windows Users
+
+    `
+    Run /<VENV FOLDER>/Scripts/activate.bat
+    `
+    or 
+    `
+    Run /<VENV FOLDER>/Scripts/Activate.ps1
+    `
--- be-server/app.py
+++ be-server/app.py
@@ -2,7 +2,15 @@ from dotenv import load_dotenv
 import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from .db.models import 
+import sqlalchemy
+from sqlalchemy.engine import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+###
+#
+# Initial Database Config
+#
+###
 
 load_dotenv()
 
@@ -10,6 +18,112 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@localhost:5432/test"'
+app.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/test'
 db = SQLAlchemy(app)
 
+
+###
+#
+# Creating the database
+#
+###
+
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/test")
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+###
+#
+# Defining the models/tables
+#
+###
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(65), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+    eco_goals = db.relationship("Eco_Goal", backref="users", lazy=True, cascade="all, delete", nullable=True)
+    eco_actions = db.relationship("Eco_Action", backref="users", lazy=True, cascade="all, delete", nullable=True)
+    categories = db.relationship("Category", backref="users", lazy=True, cascade="all, delete", nullable=False)
+    expenses = db.relationship("Expense", backref="users", lazy=True, cascade="all, delete", nullable=True)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    last_modified = db.Column(db.DateTime, default=db.func.now())
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Eco_Goal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    goal_name = db.Column(db.Text, nullable=False)
+    eco_actions = db.relationship("Eco_Action", backref="eco_goal", lazy=True, cascade="all, delete", nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    
+    def __repr__(self):
+        return '<Eco Goal %r>' % self.goal_name
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    category_name = db.Column(db.String(60), nullable=False)
+    category_description = db.Column(db.Text, nullable=True)
+    expenses = db.relationship("Expense", backref="category", lazy=True, cascade="all, delete", nullable=True)
+
+    def __repr__(self):
+        return '<Category %r>' % self.category_name
+
+class Eco_Action(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    eco_goal_id = db.Column(db.Integer, db.ForeignKey("eco_goal.id"), nullable=False)
+    expense_id = db.Column(db.Integer, db.ForeignKey("expense.id"), nullable=True)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+
+    def __repr__(self):
+        return '<Eco Action %r>' % self.id
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id"), nullable=False)
+    expense_description = db.Column(db.Text, nullable=True)
+    eco_actions = db.relationship("Eco_Action", backref="expense", lazy=True, cascade="all, delete", nullable=True)
+    amount = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    last_modified = db.Column(db.DateTime, default=db.func.now())
+
+    def __repr__(self):
+        return '<Expense %r>' % self.id
+
+
+###
+#
+# Creating the tables
+#
+###
+
+table_check = sqlalchemy.inspect(engine)
+if table_check.has_table("users") and table_check.has_table("eco_goal") and table_check.has_table("category") and table_check.has_table("eco_action") and table_check.has_table("expense"):
+    pass
+else:
+    db.create_all()
+
+###
+#
+# Defining the endpoints
+#
+###
+
+@app.route("/")
+def index():
+    return "<p>Hello World</p>"
+
+
+# russell = Users(username="Russell", password="123", email="123")
+
+query = Users.query.filter_by(username="Russell")
+
+username = query.username
+
+print(f"THE USERNAME IS:{username}")
